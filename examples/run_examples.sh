573522421826509699296dcdfe13678d3117012e
Adrian Rosebrock <jrosebr1@umbc.edu>
2015-07-03 22:48:48 +0000

    Updated example generation; added requirements.txt

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -29,3 +29,20 @@ python demo.py --base-model $CAFFE_ROOT/models/bvlc_googlenet \
 	--image initial_images/fal_04.jpg \
 	--output examples/output_fal_inception_3b_5x5_reduce_04.jpg \
 	--layer 'inception_3b/5x5_reduce'
+
+python demo.py --base-model $CAFFE_ROOT/models/bvlc_googlenet \
+	--image initial_images/fal_01.jpg \
+	--output examples/output_fal_inception_4c_output_01.jpg \
+	--layer 'inception_4c/output'
+python demo.py --base-model $CAFFE_ROOT/models/bvlc_googlenet \
+	--image initial_images/fal_02.jpg \
+	--output examples/output_fal_inception_4c_output_02.jpg \
+	--layer 'inception_4c/output'
+python demo.py --base-model $CAFFE_ROOT/models/bvlc_googlenet \
+	--image initial_images/fal_03.jpg \
+	--output examples/output_fal_inception_4c_output_03.jpg \
+	--layer 'inception_4c/output'
+python demo.py --base-model $CAFFE_ROOT/models/bvlc_googlenet \
+	--image initial_images/fal_04.jpg \
+	--output examples/output_fal_inception_4c_output_04.jpg \
+	--layer 'inception_4c/output'
